e11aa93b5188c0e6b67d0173d28371264c27e930
Daffa Muhamad Azhar <azhar.daffamuhamad@gmail.com>
2023-04-08 18:52:21 +0700

    dabug

--- app.py
+++ app.py
@@ -9,45 +9,39 @@ df_monthly = pd.read_csv('./Dataset/df_monthly.csv')
 
 min_date = df["datetime"].min()
 max_date = df["datetime"].max()
- 
-with st.sidebar:
-    # Menambahkan logo perusahaan
-    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
-    
-    # Mengambil start_date & end_date dari date_input
-    start_date = st.date_input(
-        # label='Rentang Waktu',
-        # min_value=min_date,
-        # max_value=max_date,
-        # value=[min_date, max_date]
-    )
-
 
 def main():
-    main_df = df[(df["datetime"] >= str(start_date)) & (df["datetime"] <= str(end_date))]
+    # main_df = df[(df["datetime"] >= str(start_date)) & (df["datetime"] <= str(end_date))]
     # main_df_monthly = df_monthly[(df_monthly["datetime"] >= str(start_date)) & (df_monthly["datetime"] <= str(end_date))]
 
     st.title("Bike Sharing")
     st.subheader('Jumlah Peminjam Harian')
+    
+    start_date = st.date_input("Start Date", (2011, 1, 1))
+    end_date = st.date_input("Start Date", (2012, 12, 31))
+    st.write(start_date)
+    st.write(end_date)
+
+    st.dataframe(df.head(10))
  
-    col1 = st.columns(1)
+    # col1 = st.columns(1)
     
-    with col1:
-        total_count = main_df.total_count.sum()
-        st.metric("Total Peminjam", value=total_count)
+    # with col1:
+    #     total_count = main_df.total_count.sum()
+    #     st.metric("Total Peminjam", value=total_count)
     
-    fig, ax = plt.subplots(figsize=(16, 8))
-    ax.plot(
-        main_df['datetime'],
-        main_df["total_count"],
-        marker='o', 
-        linewidth=2,
-        color="#90CAF9"
-    )
-    ax.tick_params(axis='y', labelsize=20)
-    ax.tick_params(axis='x', labelsize=15)
+    # fig, ax = plt.subplots(figsize=(16, 8))
+    # ax.plot(
+    #     main_df['datetime'],
+    #     main_df["total_count"],
+    #     marker='o', 
+    #     linewidth=2,
+    #     color="#90CAF9"
+    # )
+    # ax.tick_params(axis='y', labelsize=20)
+    # ax.tick_params(axis='x', labelsize=15)
     
-    st.pyplot(fig)
+    # st.pyplot(fig)
 
 if __name__ == '__main__':
     main()
